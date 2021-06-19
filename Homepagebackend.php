71eebd1daf2311f466876c7c1eb63da1f77258fe
AhmedsalahX <57444253+AhmedsalahX@users.noreply.github.com>
2021-06-19 23:31:01 +0200

    Add files via upload

---
+++ Homepagebackend.php
@@ -0,0 +1,12 @@
+<link href="Homepage.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&display=swap" rel="stylesheet">
+
+<?php include 'Menu.php' ?>
+
+<body>
+<div class="rectangle8"> </div>
+<div class="rectangle9"> </div>
+
+<a class="n">What's New</a>
+<a class="b">Bestseller</a>
+</body>
